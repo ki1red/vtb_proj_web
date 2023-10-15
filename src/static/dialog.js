506609acc24942066157c10f0136470771ebb755
KateSab <sabantseva1999@mail.ru>
2023-10-15 07:53:30 +0700

    подпрален чатик

--- src/static/dialog.js
+++ src/static/dialog.js
@@ -1,11 +1,13 @@
-
 // Элементы виджета
 const listMessages = document.getElementById('list_messages')
 const inputBox = document.getElementById('dialog_input')
 const sendButton = document.getElementById('button_send_message')
 
+const default_text = 'Здравствуйте! Вас приветствует бот-помощник по работе сайта. В данный момент проводятся технические работы, извините за предоставленные неудобства'
+const default_text_2 = 'Извините, в данный момент не могу обработать ваш запрос.'
+
 // Список сообщений
-let messages = []
+let messages = [default_text]
 
 // Формирование сообщения для чата
 function createMessage(inputText) {
@@ -17,10 +19,11 @@ function createMessage(inputText) {
 }
 
 // Отправка сообщения в чат
-sendButton.onclick = function(event) {
+sendButton.onclick = function (event) {
     const value = inputBox.value
     if (value.length > 0) {
         messages.push('Вы: ' + value)
+        messages.push('Бот: ' + default_text_2)
         inputBox.value = ''
     }
     updateChat()
@@ -29,8 +32,14 @@ sendButton.onclick = function(event) {
 // Обновление списка сообщений в чате
 function updateChat() {
     listMessages.innerHTML = ''
+    let html = ''
     for (const msg of messages) {
         const msgHTML = createMessage(msg)
-        listMessages.insertAdjacentHTML('afterbegin', msgHTML)
+        html += msgHTML
     }
+    listMessages.insertAdjacentHTML('beforeend', html) // Добавлен вызов insertAdjacentHTML
+
 }
+
+// Вызов функции updateChat() в конце файла
+updateChat();
